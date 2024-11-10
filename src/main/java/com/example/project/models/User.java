9b05d55f0c3e9552bd348b6257666624c3962181
lqkhanh <lequockhanh292@gmail.com>
2024-11-10 20:32:15 +0700

    add controller and model

---
+++ src/main/java/com/example/project/models/User.java
@@ -0,0 +1,18 @@
+
+package com.example.project.models;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class User {
+
+    private Long id;
+    private String name;
+    private String password;
+    private String email;
+
+}
